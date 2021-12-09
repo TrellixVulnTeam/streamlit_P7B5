b38d5bb6fcb709256c31cf70ad326c331f4a835d
adminwayra <90121073+adminwayra@users.noreply.github.com>
2021-12-08 23:40:30 -0500

    act app

--- app.py
+++ app.py
@@ -5,10 +5,20 @@ ecom_data = pd.read_csv('norte1.csv',sep=';')
 # ecom_data.dropna(inplace=True)
 
 st.set_page_config(page_title='mondelez',layout='wide')
-st.header('Mondelez Diciembre 2021')
+
+st.markdown("""
+<style>
+.big-font {
+    font-size:50px !important;
+}
+</style>
+""", unsafe_allow_html=True)
+
+st.markdown('<p class="big-font">Mondelez Diciembre 2021</p>', unsafe_allow_html=True)
+#st.header('Mondelez Diciembre 2021')
 
 proveedores=ecom_data['COD_MES'].unique().tolist()
-mes=ecom_data[' MES '].unique().tolist()
+mes=ecom_data['MES'].unique().tolist()
 mes_selection=st.sidebar.slider('Mes:',
                         min_value=min(mes),
                         max_value=max(mes),
@@ -19,26 +29,39 @@ proveedor_selection=st.sidebar.multiselect('Proveedor:',
                         proveedores,
                         default=proveedores)  
 
-mask=(ecom_data['COD_MES'].isin(proveedor_selection)) & (ecom_data[' MES '].between(*mes_selection))
+mask=(ecom_data['COD_MES'].isin(proveedor_selection)) & (ecom_data['MES'].between(*mes_selection))
 num_resul=ecom_data[mask].shape[0]  
+num_resul=ecom_data[mask].shape[0] 
 df_selection= ecom_data[mask]
-# st.dataframe(df_selection)  
-st.markdown(f'*resultado disponibles: {num_resul}*') 
-                                       
-
-
-
-#df_grouped=ecom_data.groupby(by=['NOM_PRO','COD_MES']).agg({'COB': ['mean']}).reset_index()
-#df_grouped.columns=['proveedor','supervisor','promedio']
-#df_grouped
+# st.dataframe(df_selection) 
+st.title("Coverage Dashboard") 
+st.markdown("##")
+df_col=pd.to_numeric(df_selection["DROP_MON"])
+compra_min=df_col.min(skipna=True)
+compra_max=int(df_col.max(skipna=True))
+compra_pro=int(df_col.mean(skipna=True)) 
+col1, col2, col3, col4=st.columns(4)                                    
+with col1:
+     st.subheader("Cartera activa:")
+     st.subheader(f'{num_resul:,}')
+with col2:
+     st.subheader("Compra mínima:")
+     st.subheader(f'S/ {compra_min:,}')
+with col3:
+     st.subheader("Compra máxima:")
+     st.subheader(f'S/ {compra_max:,}') 
+with col4:
+     st.subheader("Compra promedio:")
+     st.subheader(f'S/ {compra_pro:,}')       
+st.markdown("---")
 
 # ----plotear bar chart
 coverage_by_supervisor=(
-    df_selection.groupby(by=[' MES ']).sum()[[' COB ']].sort_values(by=' COB ')
+    df_selection.groupby(by=['MES']).sum()[['COB']].sort_values(by='COB')
 )
 fig_coverage_sup=px.bar(
     coverage_by_supervisor,
-    x=" COB ",
+    x="COB",
     y=coverage_by_supervisor.index,
     orientation="h",
     title="<b>Coberturas en el mes</b>",
@@ -46,18 +69,17 @@ fig_coverage_sup=px.bar(
     #template="plotly_whhite",
 )
 st.plotly_chart(fig_coverage_sup)
-
-
 pie_chart=px.pie(df_selection,
                 title='Frecuencia de compras',
-                values=' COB ',
+                values='COB',
                 names='EFE_MON')
-
-st.plotly_chart(pie_chart) 
-
 pie_chart1=px.pie(df_selection,
                 title='Compras Mensual',
-                values=' DROP_MON ',
+                values='DROP_MON',
                 names='EFE_MON')
 
-st.plotly_chart(pie_chart1) 
+col_pie1, col_pie2=st.columns(2)
+with col_pie1:
+        col_pie1.plotly_chart(pie_chart, use_container_width = True)
+with col_pie2:
+        col_pie2.plotly_chart(pie_chart1, use_container_width = True) 
