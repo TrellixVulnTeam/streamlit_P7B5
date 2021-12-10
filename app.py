3b019bd93068c7cce109770b0d5cea5a6f6b0e45
adminwayra <90121073+adminwayra@users.noreply.github.com>
2021-12-10 13:37:16 -0500

    act app

--- app.py
+++ app.py
@@ -16,70 +16,124 @@ st.markdown("""
 
 st.markdown('<p class="big-font">Mondelez Diciembre 2021</p>', unsafe_allow_html=True)
 #st.header('Mondelez Diciembre 2021')
-
-proveedores=ecom_data['COD_MES'].unique().tolist()
-mes=ecom_data['MES'].unique().tolist()
-mes_selection=st.sidebar.slider('Mes:',
+nav=st.sidebar.radio("Menu",["KPI's","Cobertura","Volumen","Vendedor"],index=0)
+if nav == "Cobertura":
+     proveedores=ecom_data['COD_MES'].unique().tolist()
+     mes=ecom_data['MES'].unique().tolist()
+     mes_selection=st.sidebar.slider('Mes:',
                         min_value=min(mes),
                         max_value=max(mes),
                         value=(min(mes),max(mes))
                         )
 
-proveedor_selection=st.sidebar.multiselect('Proveedor:',
+     proveedor_selection=st.sidebar.multiselect('Supervisor:',
                         proveedores,
                         default=proveedores)  
 
-mask=(ecom_data['COD_MES'].isin(proveedor_selection)) & (ecom_data['MES'].between(*mes_selection))
-num_resul=ecom_data[mask].shape[0]  
-num_resul=ecom_data[mask].shape[0] 
-df_selection= ecom_data[mask]
-# st.dataframe(df_selection) 
-st.title("Coverage Dashboard") 
-st.markdown("##")
-df_col=pd.to_numeric(df_selection["DROP_MON"])
-compra_min=df_col.min(skipna=True)
-compra_max=int(df_col.max(skipna=True))
-compra_pro=int(df_col.mean(skipna=True)) 
-col1, col2, col3, col4=st.columns(4)                                    
-with col1:
-     st.subheader("Cartera activa:")
-     st.subheader(f'{num_resul:,}')
-with col2:
-     st.subheader("Compra mínima:")
-     st.subheader(f'S/ {compra_min:,}')
-with col3:
-     st.subheader("Compra máxima:")
-     st.subheader(f'S/ {compra_max:,}') 
-with col4:
-     st.subheader("Compra promedio:")
-     st.subheader(f'S/ {compra_pro:,}')       
-st.markdown("---")
-
-# ----plotear bar chart
-coverage_by_supervisor=(
-    df_selection.groupby(by=['MES']).sum()[['COB']].sort_values(by='COB')
-)
-fig_coverage_sup=px.bar(
-    coverage_by_supervisor,
-    x="COB",
-    y=coverage_by_supervisor.index,
-    orientation="h",
-    title="<b>Coberturas en el mes</b>",
+     mask=(ecom_data['COD_MES'].isin(proveedor_selection)) & (ecom_data['MES'].between(*mes_selection))
+     num_resul=ecom_data[mask].shape[0]  
+     num_resul=ecom_data[mask].shape[0] 
+     df_selection= ecom_data[mask]
+     # st.dataframe(df_selection) 
+     # st.title("Coverage Dashboard") 
+     st.markdown("## Coverage Dashboard")
+     df_col=pd.to_numeric(df_selection["DROP_MON"])
+     compra_min=df_col.min(skipna=True)
+     compra_max=int(df_col.max(skipna=True))
+     compra_pro=int(df_col.mean(skipna=True)) 
+     col1, col2, col3, col4, col5=st.columns(5) 
+     with col1:
+          df_cob=df_selection.loc[df_selection['MES']==12]
+          cli_cor=df_cob.shape[0]
+          st.markdown("**Cobertura:**")
+          st.markdown(f"<h2 style='text-align: left; color: red;'>{cli_cor:,}</h2)", unsafe_allow_html=True)                                   
+     with col2:
+          st.markdown("**Cartera:**")
+          st.markdown(f"<h2 style='text-align: left; color: red;'>{num_resul:,}</h2)", unsafe_allow_html=True)
+     with col3:
+          st.markdown("Compra mín:")
+          st.markdown(f"<h2 style='text-align: left; color: red;'>{compra_min:,}</h2)", unsafe_allow_html=True)
+     with col4:
+          st.markdown("Compra máx:")
+          st.markdown(f"<h2 style='text-align: left; color: red;'>{compra_max:,}</h2)", unsafe_allow_html=True)
+     with col5:
+          st.markdown("Compra prom:")
+          st.markdown(f"<h2 style='text-align: left; color: red;'>{compra_pro:,}</h2)", unsafe_allow_html=True)       
+     st.markdown("---")
+     #st.markdown("<hr/>", unsafe_allow_html=True)
+     # ----plotear bar chart
+     coverage_by_supervisor=(
+          df_selection.groupby(by=['MES']).sum()[['COB']].sort_values(by='COB')
+          )
+     fig_coverage_sup=px.bar(
+          coverage_by_supervisor,
+          x="COB",
+          y=coverage_by_supervisor.index,
+          orientation="h",
+          title="Clientes con registro de compras",
     # color_descrete_sequence=["#008388"] * len(coverage_by_supervisor),
     #template="plotly_whhite",
-)
-st.plotly_chart(fig_coverage_sup)
-pie_chart=px.pie(df_selection,
+          )
+     st.plotly_chart(fig_coverage_sup)
+     pie_chart=px.pie(df_selection,
                 title='Frecuencia de compras',
                 values='COB',
                 names='EFE_MON')
-pie_chart1=px.pie(df_selection,
+     pie_chart1=px.pie(df_selection,
                 title='Compras Mensual',
                 values='DROP_MON',
                 names='EFE_MON')
 
-col_pie1, col_pie2=st.columns(2)
-with col_pie1:
-        col_pie1.plotly_chart(pie_chart, use_container_width = True)
-with col_pie2:
-        col_pie2.plotly_chart(pie_chart1, use_container_width = True) 
+     col_pie1, col_pie2=st.columns(2)
+     with col_pie1:
+          col_pie1.plotly_chart(pie_chart, use_container_width = True)
+     with col_pie2:
+          col_pie2.plotly_chart(pie_chart1, use_container_width = True) 
+
+if nav == "Vendedor":
+     supervisores=ecom_data['COD_MES'].unique().tolist()
+     proveedor_selection=st.sidebar.selectbox("Elije un Supervisor",
+                        supervisores,
+                        index=0)  
+     if proveedor_selection :
+          vend_list=ecom_data.loc[ecom_data['COD_MES']==proveedor_selection]
+          vendedores=vend_list['COD_VEN'].unique().tolist()
+          vendedor_selection=st.sidebar.selectbox("Elije un Vendedor",
+                        vendedores,
+                        index=0) 
+          mask=(ecom_data['COD_VEN']==vendedor_selection) 
+          num_resul=ecom_data[mask].shape[0]  
+          num_resul=ecom_data[mask].shape[0] 
+          df_selection= ecom_data[mask]          
+          #st.title("KPI's del vendedor") 
+     st.markdown("## KPI's del vendedor")
+     df_col=pd.to_numeric(df_selection["DROP_MON"])
+     compra_min=df_col.min(skipna=True)
+     compra_max=int(df_col.max(skipna=True))
+     compra_pro=int(df_col.mean(skipna=True)) 
+     col1, col2, col3, col4,col5=st.columns(5)                                    
+     with col1:
+          df_cob=df_selection.loc[df_selection['MES']==12]
+          cli_cor=df_cob.shape[0]
+          st.markdown("**Cobertura:**")
+          st.markdown(f"<h2 style='text-align: left; color: yellow;'>{cli_cor:,}</h2)", unsafe_allow_html=True)
+     with col2:
+          st.markdown("**Cartera:**")
+          st.markdown(f"<h2 style='text-align: left; color: yellow;'>{num_resul:,}</h2)", unsafe_allow_html=True)
+     with col3:
+          st.markdown("Compra mín:")
+          st.markdown(f"<h2 style='text-align: left; color: yellow;'>{compra_min:,}</h2)", unsafe_allow_html=True)
+     with col4:
+          st.markdown("Compra máx:")
+          st.markdown(f"<h2 style='text-align: left; color: yellow;'>{compra_max:,}</h2)", unsafe_allow_html=True)
+     with col5:
+          st.markdown("Compra prom:")
+          st.markdown(f"<h2 style='text-align: left; color: yellow;'>{compra_pro:,}</h2)", unsafe_allow_html=True)       
+     st.markdown("---")
+     if st.checkbox("Mostrar clientes no coberturdos"):
+          df_sel=df_selection.loc[df_selection['MES']<12]
+          df_sel=df_sel.loc[:,['COD_CLI','DIA_VIS','NOM_CLI','DIR_CLI','ULT_FEC','ULT_COM']]
+          num_cob=df_sel.shape[0]
+          st.success(f'Clientes no coberturados {num_cob:,}')
+          st.table(df_sel)
+          st.download_button(label='Download CSV',data=df_sel.to_csv(),mime='text/csv')
